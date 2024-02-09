4bdb12a69a3b878fe2787db8e361aaa10ccf34e4
Denis Vishnyakov <Vishka2005@yandex.ru>
2024-02-09 21:36:04 +0400

    open

--- plugins/modal.js
+++ plugins/modal.js
@@ -1,7 +1,39 @@
+function _createModal(options){
+    const modal = document.createElement('div')
+    modal.classList.add('vmodal')
+    modal.insertAdjacentHTML('afterbegin', `html
+        <div class="modal-overlay">
+            <div class="modal-window">
+                <div class="modal-header">
+                    <span class="modal__title">Modal</span>
+                    <span class="modal__close">&times;</span>
+                </div>
+                <div class="modal-body">
+                    <p>Lorem ipsum dolor sit.</p>
+                    <p>Lorem ipsum dolor sit.</p>
+                </div>
+                <div class="modal-footer">
+                    <button>OK</button>
+                    <button>Cancel</button>
+                </div>
+            </div>
+        </div>
+    `)
+    document.body.appendChild(modal)
+    return modal
+        
+}
+
+
 $.modal = function(options) {
+    const $modal= _createModal(options);
+
+
+
     return{
         open(){},
         close(){},
+        destroy(){}
     }
 
 }
